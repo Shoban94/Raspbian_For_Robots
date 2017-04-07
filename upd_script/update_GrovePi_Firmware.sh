7a56702eb4dc1c6fa9e46ed0b23767cc255369f2
Shoban94 <shobannarayan@gmail.com>
2017-04-07 13:30:31 +0530

    Update

--- upd_script/update_GrovePi_Firmware.sh
+++ upd_script/update_GrovePi_Firmware.sh
@@ -3,8 +3,10 @@
 echo "Beginning to Update the GrovePi Firmware!"
 echo "============================="
 
+pushd /home/pi/Dexter/GrovePi/Firmware
 source /home/pi/Desktop/GrovePi/Firmware/grovepi_firmware_update.sh
 update_grovepi_firmware
+popd
 
 echo "Finished updating the GrovePi Firmware!"
 echo "============================="
